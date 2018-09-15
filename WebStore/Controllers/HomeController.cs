010b78a872696154d630a5008fb892598b0a5542
EShop <p-sashko@mail.ru>
2018-09-15 23:25:15 +0300

    First commit. Added comment. Create repository

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace WebStore.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            //ViewData["Message"] = "Your application description page.";           
+            ViewData["Message"] = "page About.";
 
             return View();
         }
